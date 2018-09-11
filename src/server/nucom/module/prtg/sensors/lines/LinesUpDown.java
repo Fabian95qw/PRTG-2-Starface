bdeec007d6e341bbfe9b5383e5ebdee1dbf9aa7f
Fabian95qw <fabian.zuend@bluewin.ch>
2018-09-11 13:26:10 +0200

    Will now count both SIP and ISDN Lines

--- src/server/nucom/module/prtg/sensors/lines/LinesUpDown.java
+++ src/server/nucom/module/prtg/sensors/lines/LinesUpDown.java
@@ -1,15 +1,18 @@
 package nucom.module.prtg.sensors.lines;
 
-import java.util.List;
+import java.util.Collection;
 
-import de.starface.ch.processing.callactions.CallActions;
+import org.jfree.util.Log;
+
 import de.starface.core.component.StarfaceComponentProvider;
+import de.vertico.starface.config.wire.forms.WireUnitBean;
 import de.vertico.starface.module.core.model.VariableType;
 import de.vertico.starface.module.core.model.Visibility;
 import de.vertico.starface.module.core.runtime.IBaseExecutable;
 import de.vertico.starface.module.core.runtime.IRuntimeEnvironment;
 import de.vertico.starface.module.core.runtime.annotations.Function;
 import de.vertico.starface.module.core.runtime.annotations.OutputVar;
+import de.vertico.starface.persistence.connector.WireSettingsHandler;
 
 @Function(visibility=Visibility.Private, rookieFunction=false, description="Returns the Amount of Lines currently Online, and Offline")
 public class LinesUpDown implements IBaseExecutable 
@@ -29,17 +32,36 @@ public class LinesUpDown implements IBaseExecutable
 	@Override
 	public void execute(IRuntimeEnvironment context) throws Exception 
 	{
-		CallActions CA = (CallActions)context.provider().fetch(CallActions.class);
+		//CallActions CA = (CallActions)context.provider().fetch(CallActions.class);
 
-		List<String> Lines = CA.sipShowRegistry();
+		WireSettingsHandler WSH = (WireSettingsHandler)context.provider().fetch(WireSettingsHandler.class);
 		
+		Collection<WireUnitBean> Lines = WSH.getProviderConnections();
+		
+		for(WireUnitBean Line: Lines)
+		{
+			Log.debug("Line: " +Line.getDisplayName() +" ==> " +Line.getConnectionState());
+			if(Line.getConnectionState().equals("on"))
+			{
+				LinesUp++;
+			}
+			else
+			{
+				LinesDown++;
+			}
+		}
+		
+		/*
+		List<String> Lines = CA.sipShowRegistry();
+	
+			
 		if(Lines.size() == 0)
 		{
 			return;
 		}
 		
-		Lines.remove(0);
-		Lines.remove(Lines.size()-1);
+		Lines.remove(0); //Remove Header
+		Lines.remove(Lines.size()-1); //Remove Footer
 		
 		for (String Line : Lines)
 		{
@@ -53,7 +75,7 @@ public class LinesUpDown implements IBaseExecutable
 				LinesDown++;
 			}
 		}
-		
+		*/
 				
 	}//END OF EXECUTION
 
