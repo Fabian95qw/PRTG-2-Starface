fcddd9977c22d26c1e2a17b025e06a4c55a04b7c
Fabian95qw <fabian.zuend@bluewin.ch>
2022-06-20 11:31:23 +0200

    Switching away from Sigar for System Information.

--- src/server/si/module/prtg/sensors/monitoring/CPUUsageTotal.java
+++ src/server/si/module/prtg/sensors/monitoring/CPUUsageTotal.java
@@ -1,8 +1,5 @@
 package si.module.prtg.sensors.monitoring;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import de.starface.core.component.StarfaceComponentProvider;
 import de.vertico.starface.module.core.model.VariableType;
 import de.vertico.starface.module.core.model.Visibility;
@@ -14,13 +11,13 @@ import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.HardwareAbstractionLayer;
 
-@Function(visibility=Visibility.Private, rookieFunction=false, description="Returns Current CPU Usage as a Map<Core, Usage>")
-public class CPUUsage implements IBaseExecutable 
+@Function(visibility=Visibility.Private, rookieFunction=false, description="Returns Current Total CPU usage")
+public class CPUUsageTotal implements IBaseExecutable 
 {
 	//##########################################################################################
 
-	@OutputVar(label="Usage", description="",type=VariableType.MAP)
-	public Map<String, String> Usage = null;
+	@OutputVar(label="Usage", description="",type=VariableType.STRING)
+	public String UsageTotal = "";
 	
     StarfaceComponentProvider componentProvider = StarfaceComponentProvider.getInstance(); 
     //##########################################################################################
@@ -29,23 +26,16 @@ public class CPUUsage implements IBaseExecutable
 	@Override
 	public void execute(IRuntimeEnvironment context) throws Exception 
 	{	
-		Usage = new HashMap<String, String>();
-		
+
 		SystemInfo si = new SystemInfo();
 		HardwareAbstractionLayer hal = si.getHardware();
 		CentralProcessor cpu = hal.getProcessor();
 		
-		long[][] prevTicks = cpu.getProcessorCpuLoadTicks();		
-		Thread.sleep(1000);				
-		double[] loads = cpu.getProcessorCpuLoadBetweenTicks(prevTicks);	
+		long[] prevTicks = cpu.getSystemCpuLoadTicks();
+		Thread.sleep(1000);
+		double Load = cpu.getSystemCpuLoadBetweenTicks(prevTicks);
+		UsageTotal = ""+Math.round(Load*100);
 		
-		Integer Core = 0;
-		for(double d : loads)
-		{
-			Usage.put(""+Core, ""+Math.round(d*100));
-			Core++;
-		}
-				
 	}//END OF EXECUTION
 
 	
