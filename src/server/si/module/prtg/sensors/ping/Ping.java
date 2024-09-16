e1d56d42b6f765f6785c1c5324acfb02b4e92e68
Fabian95qw <fabian.zuend@bluewin.ch>
2024-09-16 14:06:14 +0200

    Fix PING Array Outboundexceptions

--- src/server/si/module/prtg/sensors/ping/Ping.java
+++ src/server/si/module/prtg/sensors/ping/Ping.java
@@ -12,126 +12,159 @@ import de.vertico.starface.module.core.runtime.annotations.InputVar;
 import de.vertico.starface.module.core.runtime.annotations.OutputVar;
 import de.vertico.starface.module.core.runtime.functions.system.Execute4;
 
-@Function(visibility=Visibility.Private, rookieFunction=false, description="")
-public class Ping implements IBaseExecutable 
+@Function(visibility = Visibility.Private, rookieFunction = false, description = "")
+public class Ping implements IBaseExecutable
 {
-	//##########################################################################################
-	
-	@InputVar(label="IPorDNS", description="", type=VariableType.STRING)
-	public String IPorDNS="";
-	
-	@InputVar(label="Timeout", description="",type=VariableType.NUMBER)
-	public Integer Timeout=5;
-	
-	@InputVar(label="PacketsperInterval", description="",type=VariableType.NUMBER)
-	public Integer PacketsperInterVal=20;
+	// ##########################################################################################
 
-	@OutputVar(label="MaxPing", description="",type=VariableType.NUMBER)
-	public Integer MaxPing=-1;
-	
-	@OutputVar(label="MinPing", description="",type=VariableType.NUMBER)
-	public Integer MinPing=-1;
-	
-	@OutputVar(label="AvgPing", description="",type=VariableType.NUMBER)
-	public Integer AvgPing=-1;
-	
-	@OutputVar(label="PaketLossPercent", description="",type=VariableType.NUMBER)
-	public Integer PacketLossPercent=-1;
-	
-	@OutputVar(label="HasTimeout", description="",type=VariableType.BOOLEAN)
-	public Boolean hasTimeout=false;
-	
-    StarfaceComponentProvider componentProvider = StarfaceComponentProvider.getInstance(); 
-    //##########################################################################################
-	
-	//###################			Code Execution			############################	
+	@InputVar(label = "IPorDNS", description = "", type = VariableType.STRING)
+	public String IPorDNS = "";
+
+	@InputVar(label = "Timeout", description = "", type = VariableType.NUMBER)
+	public Integer Timeout = 5;
+
+	@InputVar(label = "PacketsperInterval", description = "", type = VariableType.NUMBER)
+	public Integer PacketsperInterVal = 20;
+
+	@OutputVar(label = "MaxPing", description = "", type = VariableType.NUMBER)
+	public Integer MaxPing = -1;
+
+	@OutputVar(label = "MinPing", description = "", type = VariableType.NUMBER)
+	public Integer MinPing = -1;
+
+	@OutputVar(label = "AvgPing", description = "", type = VariableType.NUMBER)
+	public Integer AvgPing = -1;
+
+	@OutputVar(label = "PaketLossPercent", description = "", type = VariableType.NUMBER)
+	public Integer PacketLossPercent = -1;
+
+	@OutputVar(label = "HasTimeout", description = "", type = VariableType.BOOLEAN)
+	public Boolean hasTimeout = false;
+
+	StarfaceComponentProvider componentProvider = StarfaceComponentProvider.getInstance();
+	// ##########################################################################################
+
+	// ################### Code Execution ############################
 	@Override
-	public void execute(IRuntimeEnvironment context) throws Exception 
+	public void execute(IRuntimeEnvironment context) throws Exception
 	{
 		Logger log = context.getLog();
 
 		log.debug("HasTimeout: " + hasTimeout);
-	
+
 		Execute4 E = new Execute4();
 		E.executeAs = "Shell Command";
-		String Command = "ping "+ IPorDNS+ " -c "+ PacketsperInterVal +" -W " + Timeout +" -q | grep -e \"Name or Service not known\" -e \"packet loss\" -e \"rtt\""; 
+		String Command = "ping " + IPorDNS + " -c " + PacketsperInterVal + " -W " + Timeout
+				+ " -q | grep -e \"Name or Service not known\" -e \"packet loss\" -e \"rtt\"";
 		E.command = Command;
 		E.execute(context);
-		
-		String Result = E.output;
-		log.debug(Result);
-		log.debug(E.errorOutput);
-		log.debug(E.resultCode);
-		
-		
-		if(Result.contains("Name or service not known"))
+
+		String Output = E.output;
+		log.trace(Output);
+		log.trace(E.errorOutput);
+		log.trace(E.resultCode);
+
+		if (Output.contains("Name or service not known"))
 		{
-			hasTimeout=true;
+			hasTimeout = true;
+			PacketLossPercent = 100;
 		}
 		else
 		{
-			String[] Lines = Result.split(System.lineSeparator());
-			//########################################
-			String[] Line1 = Lines[0].split(",");
-			String Loss = Line1[2];
-			Loss =Loss.replaceAll("[^\\d.]", "");
-			PacketLossPercent = Integer.valueOf(Loss);
-			
-			//########################################
-			String[] Line2 = Lines[1].split("=");
-			//########################################
-			String[] Results = Line2[1].replaceAll(" ","").split("/");
+			String[] Lines = Output.split(System.lineSeparator());
+			log.trace("Lines: " + Lines.length);
+			if (Lines.length < 2)
+			{
+				PacketLossPercent = 100;
+				hasTimeout = true;
+				log.error("Error while determining Packet loss");
+			}
+			else
+			{
+				// ########################################
+				String[] Line1 = Lines[0].split(",");
+				log.trace("Line1: " + Line1.length);
+				if (Line1.length < 3)
+				{
+					PacketLossPercent = 100;
+					hasTimeout = true;
+					log.error("Error while reading Packet loss");
 
-			log.debug("#####################");
-			for(String S : Results)
-			{
-				log.debug(S);
+				}
+				else
+				{
+					String Loss = Line1[2];
+					Loss = Loss.replaceAll("[^\\d.]", "");
+					PacketLossPercent = Integer.valueOf(Loss);
+				}
+				// ########################################
+				String[] Line2 = Lines[1].split("=");
+				// ########################################
+				log.trace("Line2: " + Line2.length);
+				if (Line2.length < 2)
+				{
+					MinPing = -1;
+					AvgPing = -1;
+					MaxPing = -1;
+					hasTimeout = true;
+					log.error("Error while determining Ping Times");
+				}
+				else
+				{
+					String[] Results = Line2[1].replaceAll(" ", "").split("/");
+
+					if (Results.length < 3)
+					{
+						log.error("Error while reading Ping Times");
+					}
+					else
+					{
+						log.trace("#####################");
+						for (String S : Results)
+						{
+						log.trace(S);
+						}
+						// ########################################
+						String SMinPing = Results[0].replaceAll("\\.", "");
+						String SAvgPing = Results[1].replaceAll("\\.", "");
+						String SMaxPing = Results[2].replaceAll("\\.", "");
+
+						if (!SMinPing.isEmpty())
+						{
+							MinPing = Integer.valueOf(SMinPing) / 1000;
+							if(MinPing == 0)
+							{
+								MinPing=1;
+							}
+						}
+						if (!SAvgPing.isEmpty())
+						{
+							AvgPing = Integer.valueOf(SAvgPing) / 1000;
+							if(AvgPing == 0)
+							{
+								AvgPing=1;
+							}
+						}
+
+						if (!SMaxPing.isEmpty())
+						{
+							MaxPing = Integer.valueOf(SMaxPing) / 1000;
+							if(MaxPing == 0)
+							{
+								MaxPing=1;
+							}
+						}
+					}
+
+
+				}
+
+				log.trace("Min:" + MinPing);
+				log.trace("Avg: " + AvgPing);
+				log.trace("Max: " + MaxPing);
 			}
-			//########################################
-			String SMinPing = Results[0].replaceAll("\\.","");
-			String SAvgPing = Results[1].replaceAll("\\.","");
-			String SMaxPing = Results[2].replaceAll("\\.","");
-						
-			if(!SMinPing.isEmpty())
-			{
-				MinPing = Integer.valueOf(SMinPing)/1000;
-			}
-			if(!SAvgPing.isEmpty())
-			{
-				AvgPing = Integer.valueOf(SAvgPing)/1000;
-			}
-			
-			if(!SMaxPing.isEmpty())
-			{
-				MaxPing = Integer.valueOf(SMaxPing)/1000;
-			}	
-			
-			log.debug("Min:" + MinPing);
-			log.debug("Avg: " + AvgPing);
-			log.debug("Max: " + MaxPing);
 		}
 
-		
-		/*
-		Pings.addAll(PI.getPingResults());
-		
-		while(Pings.size() > NumberofPackets)
-		{
-			Pings.remove(0);
-		}
-		
-		Double DAvg = (Pings.stream().mapToInt(I -> I).average().orElse(-1.0));
-		AvgPing = DAvg.intValue();
-		MaxPing = Pings.stream().mapToInt(I -> I).max().orElse(-1);
-		MinPing = Pings.stream().mapToInt(I -> I).min().orElse(-1);
-		
-		PingStorage.Pings.put(IPorDNS, Pings);
-		log.debug("Total Stored Pings:" + Pings.size());
-		*/
-		
-	}//END OF EXECUTION
+	}// END OF EXECUTION
 
-
-	
-	
 }
